d0e5d11cfde204a8048ce93b8a4d4a9e408602e0
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-07 16:32:00 +0100

    first, middle and last name done

--- week2/nameparts.js
+++ week2/nameparts.js
@@ -1,3 +1,15 @@
 // exercise nameparts 07.02.22
-“use strict”;
 
+"use strict";
+
+const myName = "Peter Heronimous Lind";
+
+let firstName = myName.substring(myName.indexOf(0), myName.indexOf(" "));
+
+console.log(firstName);
+
+let middleName = myName.substring(myName.indexOf(" "), myName.lastIndexOf(" "));
+console.log(middleName.trim());
+
+let lastName = myName.substring(myName.lastIndexOf(" "));
+console.log(lastName.trim());

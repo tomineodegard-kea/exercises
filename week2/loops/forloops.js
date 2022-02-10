792147e0b4f86e232fa032ddcd96ae00cb8d1215
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-10 12:16:25 +0100

    is this working now

---
+++ week2/loops/forloops.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// 10.02.22 - For loop exercises, console.log the numbers
+
+// --From 0 to 9
+for (let counter = 0; counter < 10; counter++) {
+  console.log(counter);
+}
+// --From 1 to 10
+// for (let counter = 0; counter < 11; counter ++) {
+//   console.log(counter);
+// }
+
+// --From 10 to 0, and after 0 it console.logs the text "lift off"
+// for (let counter = 100; counter >= 0; counter --) {
+//   if (counter!== 0 ) {
+//     else {
+//       console.log(counter);
+//       console.log("lift off")
+//     }
+//   }
+// }
+
+// --From 1 to 19, but only counts odd numbers (no if statement, change the increment)
+// for (let counter = 1; counter < 20; counter += 2) {
+//   console.log(counter);
+// }
+
+// --From 1 to 16777216, but doubling each time (2, 4, 8, 16 ...)
+// for (let counter = 2; counter < 170000; counter *= 2) {
+//   console.log("counter:"counter);
+// }
+
+// --From 111 to 138, in steps of 3 (meaning the second number is 114)
+// for (let counter = 111; counter <= 139; counter += 3) {
+//   console.log("counter:"counter);
+// }
+
+// --From 100 down to 0, in steps of 10
+// for (let counter = 100; counter >= -10; counter -= 10) {
+//   console.log("counter:"counter);
+// }

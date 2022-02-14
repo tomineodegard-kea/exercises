5b06d9731309cc521f763240447b7bc40bdb48e9
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-14 15:36:27 +0100

    added comments

--- week3/redo_of_the_strings/fullname.js
+++ week3/redo_of_the_strings/fullname.js
@@ -1,10 +1,13 @@
 "use strict";
 
 function fullName(lastName, firstName, middleName) {
+  // if/else statement created to not get "Harry undefined Potter" if a middle name is not given
   if (middleName === undefined) {
     console.log(firstName, lastName);
   } else console.log(firstName, middleName, lastName);
 }
 
-fullName("Potter", "Harry", "James");
-fullName("Potter", "Harry");
+fullName("Potter", "Harry", "James"); // Harry James Potter
+fullName("Potter", "Harry"); // Harry Potter
+fullName("Harry", "Potter"); // Potter Harry
+fullName("Potter", "Harry", "James", "Pottypotpot"); // Harry James Potter

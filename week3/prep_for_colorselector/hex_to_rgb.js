05de5062cfd654fd3693f5f8a59ed7d7caff6f65
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-14 17:04:35 +0100

    the hexColor is now stored in an object, task complete

--- week3/prep_for_colorselector/hex_to_rgb.js
+++ week3/prep_for_colorselector/hex_to_rgb.js
@@ -1,13 +1,14 @@
 "use strict";
 
 //Convert from hex to integer
-let hexColor = "#c0ffee";
 
 function hexToRGB(hex) {
   let r = parseInt(hex.substring(1, 3), 16);
   let g = parseInt(hex.substring(3, 5), 16);
   let b = parseInt(hex.substring(5, 7), 16);
-  console.log(`This is the RGB values: Red: ${r}, Green: ${g}, Blue:${b}`);
+
+  let hexColor = { r, g, b };
+  console.log(hexColor);
 }
 
-hexToRGB(hexColor);
+hexToRGB("c0ffee");
